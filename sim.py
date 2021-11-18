ba58e3a12aaee133930646c977a1492fef03c0d4
PaulHancock <paul.hancock@curtin.edu.au>
2021-11-18 14:19:27 +0800

    create function for positions

--- sim.py
+++ sim.py
@@ -4,26 +4,41 @@
 # Determine Andromeda location in ra/dec degrees
 import numpy as np
 import math
-# from wikipedia
-ra = '00:42:44.3'
-dec = '41:16:09'
 
-d, m, s = dec.split(':')
-dec = int(d)+int(m)/60+float(s)/3600
 
-h, m, s = ra.split(':')
-ra = 15*(int(h)+int(m)/60+float(s)/3600)
-ra = ra/math.cos(dec*math.pi/180)
+def generate_positions():
+    """
+    Create 1,000 random locations within 1 degree of the Andromeda galaxy.
 
-# make 1000 stars within 1 degree of Andromeda
-ra_offsets = np.random.uniform(-1, 1, size=1000)
-dec_offsets = np.random.uniform(-1, 1, size=1000)
+    Returns
+    -------
+    ra, dec : numpy.array
+        Arrays of ra and dec coordinates in degrees.
+    """
+    # from wikipedia
+    ra = '00:42:44.3'
+    dec = '41:16:09'
 
-ras = ra + ra_offsets
-decs = dec + dec_offsets
+    d, m, s = dec.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
+
+    h, m, s = ra.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/math.cos(dec*math.pi/180)
+
+    # make 1000 stars within 1 degree of Andromeda
+    ra_offsets = np.random.uniform(-1, 1, size=1000)
+    dec_offsets = np.random.uniform(-1, 1, size=1000)
+
+    ras = ra + ra_offsets
+    decs = dec + dec_offsets
+    return ras, decs
+
+
+ras, decs = generate_positions()
 
 # now write these to a csv file for use by my other program
 with open('catalog.csv', 'w') as f:
-    print("id, ra, dec", file=f)
+    print("id,ra,dec", file=f)
     for i in range(1000):
         print("{0}, {1:7.4f}, {2:7.4f}".format(i, ras[i], decs[i]), file=f)
