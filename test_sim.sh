429b556767368062a2536e6169aef235f1aa8ac1
PaulHancock <paul.hancock@curtin.edu.au>
2021-11-22 16:02:06 +0800

    update test script to use sim_catalog

--- test_sim.sh
+++ test_sim.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 echo "Testing sim.py"
-python sim.py || { echo "FAILED to run with default parameters"; exit 1 ;}
-python sim.py --help || { echo "FAILED to print help"; exit 1 ;}
+./sim_catalog || { echo "FAILED to run with default parameters"; exit 1 ;}
+./sim_catalog --help || { echo "FAILED to print help"; exit 1 ;}
 
-python sim.py --out test.csv
+./sim_catalog --out test.csv
 if [ ! -f "test.csv" ]; then
     echo "FAILED to generate ouput test.csv"
     exit 1
