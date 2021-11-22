fe66c808b422d8e62d911e9f80dfe7311b5fc13b
PaulHancock <paul.hancock@curtin.edu.au>
2021-11-22 16:01:53 +0800

    move CLI to new script

--- skysim/sim.py
+++ skysim/sim.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python
 # Demonstrate that we can simulate a catalogue of stars on the sky
 
-# Determine Andromeda location in ra/dec degrees
 import numpy as np
 import math
-import argparse
 
 
 def generate_positions(ref_ra='00:42:44.3',
@@ -69,28 +67,3 @@ def write_file(ras, decs,
             # use a csv format
             print("{0}, {1:7.4f}, {2:7.4f}".format(i, ras[i], decs[i]), file=f)
     return
-
-
-if __name__ == '__main__':
-    # Set up the parser with all the options that you want
-    parser = argparse.ArgumentParser(prog='sim')
-    group1 = parser.add_argument_group()
-    group1.add_argument('--ref_ra', dest='ref_ra', type=str, default='00:42:44.3',
-                        help='Central/reference RA position HH:MM:SS.S format')
-    group1.add_argument('--ref_dec', dest='ref_dec', type=str, default='41:16:09',
-                        help='Central/reference Dec position DD:MM:SS.S format')
-    group1.add_argument('--radius', dest='radius', type=float, default=1.,
-                        help='radius within which the new positions are generated (deg)')
-    group1.add_argument('--n', dest='nsources', type=int, default=1_000,
-                        help='Number of positions to generate')
-    group1.add_argument('--out', dest='outfile', type=str, default='catalog.csv',
-                        help='Filename for saving output (csv format)')
-
-    # parse the command line input
-    options = parser.parse_args()
-
-    ras, decs = generate_positions(ref_ra=options.ref_ra,
-                                   ref_dec=options.ref_dec,
-                                   radius=options.radius,
-                                   nsources=options.nsources)
-    write_file(ras, decs, outfile=options.outfile)
