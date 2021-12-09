ea908b5a774c5c36bbac885dc6f21c11a892bda4
PaulHancock <paul.hancock@curtin.edu.au>
2021-12-09 10:27:13 +0800

    use napoleon for numpydoc formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,8 @@ release = 'v3.0'
 # ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
-              'sphinx.ext.viewcode'
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon',
               ]
 
 # Add any paths that contain templates here, relative to this directory.
